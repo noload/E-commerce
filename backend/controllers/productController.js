b844a460b3ec614dce78caa3bf72ee81e424cd56
noload <vaibhavkamble023@gmail.com>
2024-02-22 00:04:56 +0530

    adde product endpoint created

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,7 +1,28 @@
-const addProductController = async (req, res) => {
-  const { name, total_qnty, category, price } = req.body;
+const { Product } = require("../model");
 
-  if (!name || !total_qnty || !category || !price) {
+const addProductController = async (req, res) => {
+  try {
+    const { name, total_qnty, category, price } = req.body;
+
+    if (!name || !total_qnty || !category || !price) {
+      res.status(404).json({
+        success: false,
+        message: "All field required",
+      });
+    }
+
+    const product = await Product.create(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "something went wrong in add product controller",
+    });
   }
 };
 
